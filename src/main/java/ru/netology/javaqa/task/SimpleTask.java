22b31b7188278efda9e268534c8b383dfaf17ce6
Julia <ulia63016@gmail.com>
2024-01-06 20:31:29 +0300

    Add tests and files to create planner

---
+++ src/main/java/ru/netology/javaqa/task/SimpleTask.java
@@ -0,0 +1,15 @@
+package
+
+public class SimpleTask extends Task {
+    protected String title;
+
+    public SimpleTask(int id, String title) {
+        super(id); // вызов родительского конструктора
+        this.title = title; // заполнение своих полей
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
